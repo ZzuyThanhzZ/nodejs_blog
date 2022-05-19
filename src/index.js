1d429b67365f2e87c351d89d0c969cbb43b416fc
thanhnd <thanhnd@visionnetwork.vn>
2022-05-19 11:23:30 +0700

    remove public.css folder

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const morgan = require("morgan");
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//HTTP Logger
+// app.use(morgan('combined'));
+
+//Template engine
+app.engine('hbs', handlebars.engine({
+    extname: '.hbs',
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resource/views'));
+
+app.get("/", (req, res) => {
+    res.render('home');
+});
+
+app.get("/news", (req, res) => {
+    res.render('news');
+})
+
+app.listen(port, () => console.log("Gello"));
